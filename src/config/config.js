ebb5db609a919e63f5f1f7bdf7de8f10c8f83f3b
lph <780376964@qq.com>
2020-03-11 18:01:20 +0800

    修改图片浅醉

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const Config = {
    constant: {
       // img_base_url: 'http://m.cq2win.com:9999/net-medical/picture/'
       //  img_base_url: '/res/',
-      img_base_url:  'development'?'http://192.168.0.11:9999/net-medical/picture/':'http://m.cq2win.com:9999/net-medical/picture/',
+      img_base_url: process.env.NODE_ENV == 'development' ? 'http://192.168.0.11:9999/net-medical/picture/' : 'http://m.cq2win.com:9999/net-medical/picture/',
    }
 };
 
